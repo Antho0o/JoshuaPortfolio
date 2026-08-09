7af94d9e3bae21effb87e248671fac8375b846c5
Antho0o <st10439041@myemeris.edu.za>
2026-08-09 14:38:08 +0200

    Fix Render file watcher

--- JoshuaPortfolio/Program.cs
+++ JoshuaPortfolio/Program.cs
@@ -6,12 +6,6 @@ namespace JoshuaPortfolio
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Disable configuration file reload watchers.
-            builder.Configuration.Sources
-                .OfType<Microsoft.Extensions.Configuration.Json.JsonConfigurationSource>()
-                .ToList()
-                .ForEach(source => source.ReloadOnChange = false);
-
             var app = builder.Build();
 
             app.UseDefaultFiles();
